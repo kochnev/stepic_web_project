bc87e8ea108f177f7efcd0dbdd0e7c56419a6738
AlexanderK <lyotchik1@mail.ru>
2016-03-05 22:59:58 +0300

    async_server.py added

---
+++ async_server.py
@@ -0,0 +1,31 @@
+import socket
+import threading
+
+class MyThread(threading.Thread):
+    def __init__(self, channel, details):
+        self.channel = channel
+        self.details = details
+        threading.Thread.__init__(self)
+    
+    def run(self):
+        print("Recieved connection:" + self.details[0])
+        data = self.channel.recv(1024)
+        if data:
+            if data != 'close':
+                self.channel.send(data)
+                
+        self.channel.close()
+        print("Closed connection:" + self.details[0])
+                       
+
+#set server
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(('0.0.0.0', 2222))
+s.listen(10)
+
+
+while True:
+    conn, addr = s.accept()
+    MyThread(conn, addr).start()
+    
+    
